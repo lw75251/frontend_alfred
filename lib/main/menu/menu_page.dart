4ff36dc44aa4b74893c6d53da065dab4e0113c39
Leon <lw75251@gmail.com>
2019-12-27 19:12:56 -0500

    Before Web Transition

---
+++ lib/main/menu/menu_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// class MenuScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xFF21BFBD),
+//       appBar: AppBar(
+//         leading: BackButton(color: Colors.white,),
+//         backgroundColor: Colors.black,
+//         title: Text("Rigos", style: style),
+//         bottom: TabBarNoRipple(
+//           controller: _tabController,
+//           isScrollable: true,
+//           labelColor: Colors.black,
+//           unselectedLabelColor: Colors.white,
+//           indicator: BubbleTabIndicator(
+//             indicatorHeight: 25.0,
+//             indicatorColor: Colors.white,
+//             tabBarIndicatorSize: TabBarIndicatorSize.tab
+//           ),
+//           tabs: createTabs(menu)
+//         ),
+//       ),
+//     );
+//   }
+// }
